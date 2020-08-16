761a4e97293b22d14fb13425da5540ce5e32d0ec
sefx5ever <sefx5ever@gmail.com>
2020-08-16 22:27:31 +0800

    delete something important

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Request Link and Long Polling Use
-TELEGRAM_TOKEN = '1269949154:AAFmbWPHBHuFrA8YTECKTcQIQlZOp249gPg'
-WEBHOOK_URL = 'https://ctszkhgua8.execute-api.us-east-1.amazonaws.com/default/Telegram_Processor'
+TELEGRAM_TOKEN = ''
+WEBHOOK_URL = ''
 TELEGRAM_BASE = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}'
 TELEGRAM_WEBHOOK_URL = TELEGRAM_BASE + f'/setWebhook?url={WEBHOOK_URL}/hook'
-FUGLE_API_TOKEN = '099da7e7fbdbc38939db462c2f546ff0'
+FUGLE_API_TOKEN = ''
 
 # Other Chatbot Token
-EXAMPLE_TOKEN = '1038763893:AAG98HojBnEIuUgYbY7IBgyNmXoGjT2hvjM'
+EXAMPLE_TOKEN = ''
 
-# WEBHOOK_URL = 'https://ctszkhgua8.execute-api.us-east-1.amazonaws.com/default/Telegram_Processor'
+# WEBHOOK_URL = ''
 
 
 
